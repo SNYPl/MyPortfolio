192665cb94104c214b52c8f2cfe8bca3ff05a379
Daviti Metreveli <44302299+SNYPl@users.noreply.github.com>
2020-07-17 16:17:44 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+
+document.querySelector(".smoothScroll").addEventListener("click", function () {
+console.log("true")
+// document.getElementById("rotate").classList.toggle("csstransforms3d");
+
+
+});
