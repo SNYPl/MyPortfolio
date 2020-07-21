c3e3f60b4a286c5fb10da122b0471fc55bbda33d
Daviti Metreveli <44302299+SNYPl@users.noreply.github.com>
2020-07-21 15:58:52 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
 
+window.addEventListener("scroll",function() {
+  let links =  document.querySelectorAll(".navigation a");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.querySelector(".navigation").style.backgroundColor = "rgba(70, 123, 91, 1)";
+       
+        links.forEach(function (item) {
+
+          item.style.marginRight = "18px";
+          item.style.paddingTop = "12px"
+          
+      });
+       
+       
+      } else {
+             
+        document.querySelector(".navigation").style.backgroundColor = "";
+
+
+        links.forEach(function (item) {
+
+          item.style.marginRight = "13px";
+          item.style.paddingTop = "4px"
+          
+      });
+
+        
+      }
+
+
+});
+
 
 
 window.addEventListener("scroll",function() {
-
+  let links =  document.querySelectorAll(".navigation a");
 
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.querySelector(".navigation").style.backgroundColor = "#4dc47d";
-        document.querySelector(".navigation a").style.marginBottom = "30px";
+       
+          
+      
+       
+       
       } else {
-        document.querySelector(".navigation").style.backgroundColor = "";
+             
+       
+      
+
+        
       }
-
-console.log("true");
-
 
 
 });
